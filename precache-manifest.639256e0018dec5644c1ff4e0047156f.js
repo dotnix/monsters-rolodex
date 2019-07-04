b753ab7aab2488405f7bb58c757803e7d08635b1
Erick Matsuzaki Cechelero <erick.cechelero@gmail.com>
2019-07-04 11:57:38 -0300

    Updates

---
+++ precache-manifest.639256e0018dec5644c1ff4e0047156f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b0f1cee731b8462b7ba351dacae9dc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c46abe85fe7dbf32e8e0",
+    "url": "/monsters-rolodex/static/css/main.d4c4d115.chunk.css"
+  },
+  {
+    "revision": "1373109ea0706e1dc260",
+    "url": "/monsters-rolodex/static/js/2.8c8eca74.chunk.js"
+  },
+  {
+    "revision": "c46abe85fe7dbf32e8e0",
+    "url": "/monsters-rolodex/static/js/main.227f2b37.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
